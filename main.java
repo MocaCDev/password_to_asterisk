7fa38a6825221c8235d8819f9113127fc61b812d
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-08-12 12:26:21 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class Main {
+
+  public static ArrayList<String> got_data = new ArrayList<String>();
+
+  public static void main(String[] args) {
+  }
+
+}
