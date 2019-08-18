6d82d6470d327e8f9c69d0b29a228130e373ee4e
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-08-17 19:09:47 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,13 +1,22 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Formatter;
+import set_it_up.set_it_up.set_it_up;
 import java.util.Scanner;
 
 public class Main {
-
-  public static ArrayList<String> got_data = new ArrayList<String>();
-
-  public static void main(String[] args) {
+  public static void main(String[] args) throws StringIndexOutOfBoundsException, Exception {
+    Scanner get_password = new Scanner(System.in);
+    set_it_up setup = new set_it_up() {
+      @Override 
+      public void get_setup(String pas) {
+        password(pas);
+      }
+    };
+    System.out.print("PASSWORD: ");
+    String password = get_password.nextLine();
+    if(password.length() > 21) {
+      throw new StringIndexOutOfBoundsException("Password is too long");
+    } else {
+      System.out.println("\n\n\n");
+      setup.get_setup(password);
+    }
   }
-
 }
