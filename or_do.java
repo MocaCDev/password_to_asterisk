b729d193b094a4386a33a3359ad8286a61036854
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-08-23 12:46:59 -0500

    Update or_do.java

--- or_do.java
+++ or_do.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 
+public class or_do {
+  public static void main(String[] args) {
+    Scanner get_pass = new Scanner(System.in);
+    String password = get_pass.nextLine();
+    
+    char[] repl = new char[password.Length];
+    
+    for(int i = 0; i < repl.Length; i++) {
+      repl[i] = '*';
+    }
+    
+    System.out.println(repl);
+  }
+}
